af8eb9fe73fa666cafae79329cae6655ddd7843a
ncc <ncc@ccs.neu.edu>
2014-07-15 16:24:52 -0400

    Skeleton

---
+++ manifests/apt.pp
@@ -0,0 +1,39 @@
+define freight::apt (
+  $location,
+  $repo,
+  $release        = $::lsbdistrelease,
+  $manage_key     = false,
+  $key            = undef,
+  $source         = undef
+) {
+  # Nearly never hurts to over test
+  if $::osfamily != 'Debian' {
+    fail('This module only works on Debian or derivatives like Ubuntu')
+  }
+  
+  validate_bool($manage_key)
+
+  # Suck in apt
+  require ::apt
+
+  # If we're managing the repo key, validate things
+  if $manage_key {
+    if $key == undef {
+      fail('Define[\'freight::apt\']: key undefined')
+    }
+    if $source == undef {
+      fail('Define[\'freight::apt\']: source undefined')
+    }
+
+    apt::key { $name:
+      key        => $key,
+      key_source => $source,
+      notify     => Apt::Source[ $name ]
+    }
+  }
+  apt::source { $name:
+    location   => $location,
+    release    => $release,
+    repos      => $repo
+  }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -11,12 +11,50 @@
 # Sample Usage:
 #
 class freight (
-  $varcache = $freight::params::varcache,
-  $varlib   = $freight::params::varlib,
-  $origin   = $freight::params::origin,
-  $cache    = $freight::params::cache,
-  $symlinks = $freight::params::symlinks,
-  $gpg      = $freight::params::gpg
+  $version       = 'installed',
+  $varcache      = $freight::params::varcache,
+  $varlib        = $freight::params::varlib,
+  $origin        = $freight::params::origin,
+  $cache         = $freight::params::cache,
+  $symlinks      = $freight::params::symlinks,
+  $gpg_email     = $freight::params::gpg_email,
+  $manage_apt    = true,
+  $apt_label     = $freight::params::apt_label,
+  $apt_key       = $freight::params::key,
+  $apt_ksource   = $freight::params::key_source,
+  $apt_location  = $freight::params::apt_location,
+  $apt_release   = $freight::params::apt_release,
+  $apt_repo      = $freight::params::apt_repo,
+  $manage_apache = true
 ) inherits freight::params {
+  if $::osfamily != 'Debian' {
+    fail('This module only works on Debian or derivatives like Ubuntu')
+  }
+  validate_bool($manage_apt)
+  validate_bool($manage_apache)
   
+  # Version validation - needs improvement
+  validate_re($version, 'present|installed|latest|^[.+_0-9a-zA-Z:-]+$')
+  
+  if $manage_apt {
+    freight::apt { $apt_label: 
+      location   => $apt_location,
+      repo       => $apt_repo,
+      release    => $apt_release,
+      manage_key => true,
+      key        => $apt_key,
+      source     => $apt_ksource,
+      notify     => Package['freight']
+    }
+  }
+  
+  if $manage_apache {
+    package { 'freight': 
+      ensure => $version
+    }
+  } else {
+    package { 'freight': 
+      ensure => $version 
+    }
+  }
 }
